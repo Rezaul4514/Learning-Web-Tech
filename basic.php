d16f7d9f6c293a23a6aea415d65eb4a958735664
Rezaul4514 <grabby1730141@gmail.com>
2025-08-05 22:45:13 +0600

    Initial DOM manupulation

---
+++ basic.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- Tells the browser this is an HTML5 document -->
+
+<html>
+
+
+<head>
+  
+
+    <title>My Web Page</title>
+ 
+</head>
+
+<body>
+
+
+<h1>Hello!</h1>
+
+<p>This is a simple web page.</p>
+    
+
+</body>
+
+</html>
